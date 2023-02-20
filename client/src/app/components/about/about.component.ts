c94605c8bb10dd2fe7d2c4198481176553e36421
lestersanjuan <lsanjua1@uci.edu>
2023-02-20 12:20:33 -0800

    FIXED aboutme components

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,35 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
-import { ArtistData } from '../../data/artist-data';
-import { AlbumData } from '../../data/album-data';
-import { TrackData } from '../../data/track-data';
-import { ResourceData } from '../../data/resource-data';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css'],
-  providers: [SpotifyService]
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
-export class SearchComponent implements OnInit {
-  searchString: string;
-  searchCategory: string = 'artist';
-  searchCategories: string[] = ['artist', 'album', 'track'];
-  resources: ResourceData[];
+export class AboutComponent implements OnInit {
+  name: string = null;
+  profile_pic: string = "../../../assets/unknown.jpg";
+  profile_link: string = null;
 
-  constructor(private spotifyService: SpotifyService) { }
+  //TODO: inject the Spotify service
+  constructor(private spotify: SpotifyService) { }
 
   ngOnInit() {
   }
+  aboutMe(): void {
+    this.spotify.aboutMe().then((data) => {
+      this.name = data.name;
+      this.profile_pic = data.imageURL;
+      this.profile_link = data.spotifyProfile;
+    });
 
-  searchSongs() {
-    console.log(this.searchCategory);
-    console.log(this.searchString);
-    this.spotifyService.searchFor(this.searchCategory, this.searchString).then((data) => {
-      console.log(data);
-      console.log("lester");
-      this.resources = data;
-    })
   }
+  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
+  In that function, update the name, profile_pic, and profile_link fields */
 
 }
